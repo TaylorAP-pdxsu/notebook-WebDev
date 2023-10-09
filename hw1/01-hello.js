7c0e791dde2d9f87a66fe50195436fdc21790d42
Taylor <tapet2@pdx.edu>
2023-10-08 22:27:48 -0700

    Added alt text to img. Added printBio function to .js file

--- hw1/01-hello.js
+++ hw1/01-hello.js
@@ -1,5 +1,12 @@
-window.addEventListener('load', loadPage);
+window.addEventListener("load", loadPage);
+window.addEventListener("load", printBio);
 
 function loadPage(event) {
-  console.log('The page has now loaded.');
+  console.log("The page has now loaded.");
+}
+
+function printBio(event) {
+  console.log(
+    "My name is Taylor Pettingill. I am a Computer Science student and plan to graduate at the end of this school year."
+  );
 }
