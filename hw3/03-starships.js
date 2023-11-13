0464ff18fb03aecfb63a0492ed7980f05d728260
Taylor <tapet2@pdx.edu>
2023-11-12 23:46:08 -0800

    complete 03-starships

--- hw3/03-starships.js
+++ hw3/03-starships.js
@@ -1,5 +1,5 @@
 const url = "https://swapi.dev/api/starships/";
-let starships = [{ name: "" }];
+let starships = [{}];
 
 const fetchData = (url) => {
   // REtrieve the data from the API
@@ -18,18 +18,22 @@ fetchData(url);
 const createSpaceshipComponent = (starship) => {
   const container = document.createElement("section"); // do not modify this line
 
+  //ship name
   let shipName = document.createElement("div");
   shipName.className = "ship-name";
   container.append(shipName);
 
+  //ship price
   let shipPrice = document.createElement("div");
   shipPrice.className = "ship-price";
   container.append(shipPrice);
 
+  //ship manufacturer
   let shipManufacturer = document.createElement("div");
   shipManufacturer.className = "ship-manufacturer";
   container.append(shipManufacturer);
 
+  //ship max atmosphere speed
   let shipAtmoSpeed = document.createElement("div");
   shipAtmoSpeed.className = "ship-atmo-speed";
   container.append(shipAtmoSpeed);
@@ -37,6 +41,7 @@ const createSpaceshipComponent = (starship) => {
   atmoSpeedText.className = "atmo-speed-text";
   container.append(atmoSpeedText);
 
+  //ship cargo capacity
   let shipCargoCap = document.createElement("div");
   shipCargoCap.className = "ship-cargo-cap";
   container.append(shipCargoCap);
@@ -44,12 +49,17 @@ const createSpaceshipComponent = (starship) => {
   cargoCapText.className = "cargo-cap-text";
   container.append(cargoCapText);
 
+  //add text content to elements
   shipName.textContent = starship.name;
-  shipPrice.textContent = starship.cost_in_credits + " credits";
+  shipPrice.textContent = `${parseInt(
+    starship.cost_in_credits
+  ).toLocaleString()} credits`;
   shipManufacturer.textContent = starship.manufacturer;
   shipAtmoSpeed.textContent = starship.max_atmosphering_speed;
   atmoSpeedText.textContent = "Max atmosphering speed";
-  shipCargoCap.textContent = starship.cargo_capacity;
+  shipCargoCap.textContent = `${parseInt(
+    starship.cargo_capacity
+  ).toLocaleString()}`;
   cargoCapText.textContent = "Cargo Capacity";
 
   return container; // do not modify this line
@@ -59,11 +69,28 @@ const main = document.getElementsByTagName("main")[0];
 
 const filterStarships = (input) => {
   // Return an array with all ships that have less than 10 passengers with more than one crew member
+  let filterArray = new Array();
+  let maxPassengers = 10;
+
+  for (let i = 0; i < input.length; i++) {
+    if (input[i].passengers < maxPassengers) {
+      let minCrew = parseInt(input[i].crew.slice(0, 2)); //not sure if necessary to slice, but some ships have crew listed x-y
+      if (minCrew > 0) {
+        filterArray.push(input[i]);
+      }
+    }
+  }
+  return filterArray;
 };
 
 const reduceStarships = (input) => {
   // Return a String of the cost to purchase all ships in the input array
-  const totalCost = 0;
+  let totalCost = 0;
+  for (let i = 0; i < input.length; i++) {
+    if (!isNaN(input[i].cost_in_credits)) {
+      totalCost += parseInt(input[i].cost_in_credits);
+    }
+  }
 
   return `The cost of all starships is ${totalCost.toLocaleString()} credits`;
 };
